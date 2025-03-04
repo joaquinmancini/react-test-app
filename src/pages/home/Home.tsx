f0283d9d478f3468ce084dd8f0de1a6e2e5343e8
Emilio Ferraro <eferraro@quadiontech.com>
2025-03-03 23:00:39 -0300

    refactor en home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,19 +1,14 @@
-import './Home.css'
 import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { getCarsMock } from '../../services/mock/CarsMock.ts'
-import {
-  Auto,
-  CarsResponse,
-  useGetAutosService
-} from '../../services/service.ts'
+import { CarsResponse, useGetAutosService } from '../../Services/service'
+import './Home.css'
 const Home: React.FC = () => {
   const [cars, setCars] = useState<CarsResponse>()
   const [isLoading, setIsLoading] = useState(false)
+
   // const [user, setUser] = useState(Promise<Auto[]>);
   // const navigate = useNavigate()
   // const carsitos = getAutos()
-  const { getAutos } = useGetAutosService()
+  const { getAutos, getAutosServ } = useGetAutosService()
 
   const getAutosFn = async () => {
     const cars = await getAutos()
@@ -21,23 +16,26 @@ const Home: React.FC = () => {
     return cars
   }
 
-  // useEffect(() => {
-  //   setIsLoading(true)
-  //   const miguel: Promise<CarsResponse> = getAutosServ()
-  //   const autos = await miguel
-  //   if (!autos) {
-  //     //  showModal(ErrorService)
-  //     console.log('Se produjo u nerror al cargar los autos')
-  //   }
-  //   setCars(autos)
-  //   setIsLoading(false)
-  // }, [getAutos])
+  useEffect(() => {
+    if (cars) {
+      console.log('Trayendo autos del servicio!')
+    }
+  }, [cars])
 
   useEffect(() => {
-    console.log('consologeando');
-    
-    getAutosFn()
-  }, [])
+    const llamado = async () => {
+      setIsLoading(true)
+      const miguel: Promise<CarsResponse> = getAutosServ()
+      const autos = await miguel
+      if (!autos) {
+        //  showModal(ErrorService)
+        console.log('Se produjo un error al cargar los autos')
+      }
+      setCars(autos)
+      setIsLoading(false)
+    }
+    llamado()
+  }, [getAutos])
 
   return (
     <>
